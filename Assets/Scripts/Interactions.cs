8e31923151267b1b6ca309dc1bfaccb3da6c125e
Brandon-Reichman <breich5@uic.edu>
2017-09-20 15:11:14 -0500

    Wireframe Rotation
    I created a scene where theres is a wireframe of a cube. When holding
    the left grip trigger the cube will match the roation of your hand.
    Pressing A resets the rotation. Pressing X and Y will switch between a
    wireframe of a cube and a wireframe of a hypercube projection.

---
+++ Assets/Scripts/Interactions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour {
+
+	public OVRInput.Controller Lcontroller,Rcontroller;
+	public GameObject righthand,threeframe,fourframe;
+
+	private Quaternion RotStart;
+	private Vector3 PosStart,Rhand;
+
+	void Start()
+	{
+		RotStart = threeframe.transform.localRotation;
+		PosStart = threeframe.transform.localPosition;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (OVRInput.Get (OVRInput.RawButton.LHandTrigger)) 
+		{
+			threeframe.transform.localRotation = OVRInput.GetLocalControllerRotation (Lcontroller);
+			fourframe.transform.localRotation = OVRInput.GetLocalControllerRotation (Lcontroller);
+			//this.transform.localPosition = (OVRInput.GetLocalControllerPosition (Lcontroller))+(PosStart);
+		}
+
+		if (OVRInput.Get (OVRInput.RawButton.RHandTrigger))
+		{
+
+		}
+
+		if (OVRInput.Get(OVRInput.RawButton.Y)) {
+			fourframe.SetActive (false);
+			threeframe.SetActive(true);
+		}
+		if (OVRInput.Get(OVRInput.RawButton.X)) {
+			fourframe.SetActive(true);
+			threeframe.SetActive(false);
+		}
+
+
+		//reset position and rotation
+		if (OVRInput.Get (OVRInput.RawButton.A)) {
+			threeframe.transform.localRotation = RotStart;
+			threeframe.transform.localPosition = PosStart;
+
+			fourframe.transform.localRotation = RotStart;
+			fourframe.transform.localPosition = PosStart;
+		}
+
+	}
+}
