b99f9ea15f74d472b3f9189f728cc75df19fad4a
Brandon-Reichman <breich5@uic.edu>
2017-10-03 18:45:00 -0500

    Read Obj files

--- Assets/Scripts/Reading.cs
+++ Assets/Scripts/Reading.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class Reading : MonoBehaviour {
 
+	public GameObject mesh;
+
 	private string[] file;
+	private string[] verts;
+	private int j=0,k=0;
 	// Use this for initialization
 	void Start () 
 	{
-		file = System.IO.File.ReadAllLines (@"C:\Users\breich5\Documents\cube.txt");
-		//System.IO.File.Move ("cube.obj", "cube.txt");
+		
+		file = System.IO.File.ReadAllLines (@"C:\Users\breich5\Documents\cube.obj");
+		for (int i=0;i<file.Length;i++) {
+			if (file [i].Trim ().StartsWith ("v") && !(file [i].Trim ().StartsWith ("vn"))) {
+				j++;
+			}
+		}
+		verts = new string[j];
+		for (int i=0;i<file.Length;i++) {
+			if (file [i].Trim ().StartsWith ("v") && !(file [i].Trim ().StartsWith ("vn"))) {
+				verts [k] = file [i];
+				k++;
+			}
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
+
 		if (Input.GetKeyDown (KeyCode.P)) {
-			foreach (string line in file)
-				Debug.Log ("\t" + line);
+			foreach (string line in verts)
+				Debug.Log (line);
 		}
 	}
 }
