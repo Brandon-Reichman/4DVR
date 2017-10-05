83568d1f5a14d67aae52428ac83378461613905f
Brandon-Reichman <breich5@uic.edu>
2017-10-04 20:25:48 -0500

    Attempt to split coordinates

--- Assets/Scripts/Reading.cs
+++ Assets/Scripts/Reading.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +10,8 @@ public class Reading : MonoBehaviour {
 
 	private string[] file;
 	private string[] verts;
+	private string[] numbers;
+	private float[] nums;
 	private int j=0,k=0;
 	// Use this for initialization
 	void Start () 
@@ -20,20 +24,41 @@ public class Reading : MonoBehaviour {
 			}
 		}
 		verts = new string[j];
+		numbers = new string[j*4];
+		nums = new float[j * 4];
 		for (int i=0;i<file.Length;i++) {
 			if (file [i].Trim ().StartsWith ("v") && !(file [i].Trim ().StartsWith ("vn"))) {
 				verts [k] = file [i];
 				k++;
 			}
 		}
+		for (int i = 0; i < verts.Length; i++) {
+			verts [i] = verts [i].Remove (0, 2);
+		}
+
+		//this for loop is supposed to split each component of the vertex
+		for (int i = 0; i < numbers.Length; i++){
+			numbers = verts [i].Split (new char[]{' '});
+			nums[i] = Single.Parse(numbers[i]);;
+		}
+
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
 
+
 		if (Input.GetKeyDown (KeyCode.P)) {
 			foreach (string line in verts)
+				Debug.Log (line);
+		}
+		if (Input.GetKeyDown (KeyCode.O)) {
+			foreach (string line in numbers)
+				Debug.Log (line);
+		}
+		if (Input.GetKeyDown (KeyCode.N)) {
+			foreach (float line in nums)
 				Debug.Log (line);
 		}
 	}
