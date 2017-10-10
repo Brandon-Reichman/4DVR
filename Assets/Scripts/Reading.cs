1d8c97e06528748b151d9aa86460e0d166406777
Brandon-Reichman <breich5@uic.edu>
2017-10-09 20:35:16 -0500

    Load verts from obj file to array of Vector4
    done in reading.cs in a quite chaotic way...

--- Assets/Scripts/Reading.cs
+++ Assets/Scripts/Reading.cs
@@ -9,23 +9,24 @@ public class Reading : MonoBehaviour {
 
 	public GameObject mesh;
 
+	private Vector4[] vertices;
 	private string v,location;
-	private string[] file,verts,numbers;
-	private float[] nums;
-	private int j=0,k=0;
+	private string[] file,verts;
+	private int j=0,k=0,l=0;
 	// Use this for initialization
 	void Start () 
 	{
 		location = AssetDatabase.GetAssetPath (mesh);
 		file = System.IO.File.ReadAllLines (location);
+		//counting verts to allocate array size
 		for (int i=0;i<file.Length;i++) {
 			if (file [i].Trim ().StartsWith ("v") && !(file [i].Trim ().StartsWith ("vn"))) {
 				j++;
 			}
 		}
+		vertices = new Vector4[j];
 		verts = new string[j];
-		numbers = new string[j*4];
-		nums = new float[j * 4];
+		//loading vertex value from file
 		for (int i=0;i<file.Length;i++) {
 			if (file [i].Trim ().StartsWith ("v") && !(file [i].Trim ().StartsWith ("vn"))) {
 				verts [k] = file [i].Remove(0,2);
@@ -33,31 +34,38 @@ public class Reading : MonoBehaviour {
 				k++;
 			}
 		}
-		v = String.Join ("", verts);
-		for (int i = 0; i < numbers.Length; i++){
-		  	numbers = v.Split (new char[]{' '});
-			nums[i] = float.Parse(numbers[i]);
+		//most absurd loding of vectors possible...
+		for (int i = 0; i < j; i++) {
+			int h = 0;
+			string p = verts[i];
+			float[] ok = new float[4];
+			string[] e = p.Split (new char[]{ ' ' });
+			for (int q = 0; q < 4; q++)
+				ok [q] = float.Parse (e [q]);
+			vertices [i].x = ok [h];
+			vertices [i].y = ok [h+1];
+			vertices [i].z = ok [h+2];
+			vertices [i].w = ok [h+3];
+			h++;
+			if (h > 4)
+				h = 0;
 		}
-
-	}
+		/*v = String.Join ("", verts);
+		numbers = v.Split (new char[]{' '});
+		for (int i = 0; i < numbers.Length; i++)
+			nums[i] = float.Parse(numbers[i]);*/
+		}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.V)) 
-				Debug.Log (v);
-
 		if (Input.GetKeyDown (KeyCode.P)) {
 			foreach (string line in verts)
 				Debug.Log (line);
 		}
-		if (Input.GetKeyDown (KeyCode.O)) {
-			foreach (string line in numbers)
-				Debug.Log (line);
-		}
-		if (Input.GetKeyDown (KeyCode.N)) {
-			foreach (float line in nums)
-				Debug.Log (line);
+		if (Input.GetKeyDown (KeyCode.F)) {
+			foreach (Vector4 v in vertices)
+				Debug.Log (v);
 		}
 	}
 }
